61951976462a9ae819e4799313c5f87c881dbc06
Luciana Maroun <lucianamaroun@gmail.com>
2014-07-30 13:59:21 -0300

    Modifications on output of experiments for visualization.

--- experiments/time.py
+++ experiments/time.py
@@ -1,6 +1,6 @@
 _TIME_DIR = 'time/'
 
-def time_measure(random_iters, repetitions):
+def time_avg(random_iters, repetitions):
   time_avgs = []
   for it in random_iter:
     time_file = open(_TIME_DIR + 'time%d.dat' % it, 'r')
@@ -18,11 +18,26 @@ def time_measure(random_iters, repetitions):
     time_avgs.append((disamb_time / rep, worlds_time / rep, rank_time / rep))
   return time_avgs
 
+def time_measure(random_iters, repetitions):
+  time_measures = []
+  for it in random_iter:
+    time_file = open(_TIME_DIR + 'time%d.dat' % it, 'r')
+    for rep in range(repetitions):
+      time_file.readline()
+      disamb_time = float(time_file.readline().strip())
+      time_file.readline()
+      worlds_time = float(time_file.readline().strip())
+      time_file.readline()
+      rank_time = float(time_file.readline().strip())
+      time_file.readline()
+      time_measures.append((it, (disamb_time, worlds_time, rank_time)))
+  return time_measures
+
 if __name__ == '__main__':
 
     random_iter = [1, 2, 5, 10, 22, 46, 100, 215, 464, 1000]
-    repetitions = 3
+    repetitions = 5
 
-    time_avgs = time_measure(random_iter, repetitions)
-    for i, time in enumerate(time_avgs):
-      print '%d,%d,%d,%d' % (random_iter[i], time[0], time[1], time[2])
+    time_measures = time_measure(random_iter, repetitions)
+    for it, time in time_measures:
+      print '%d,%d,%d,%d' % (it, time[0], time[1], time[2])
